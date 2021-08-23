e3f66c81b739c5629043c370f738b6cfd6e80320
Stefen Hasselberg <Stefen.Hasselberg@richmond.org.nz>
2021-08-23 12:44:26 +1200

    initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+NodeJS ExpressJS and Typescript project
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello from the typescript world!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
